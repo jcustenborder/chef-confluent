9d692241b91b623289f716bcde509c2e9179f131
Jeremy Custenborder <jcustenborder@gmail.com>
2017-12-08 11:08:09 -0500

    Add generated delivery build cookbook

---
+++ .delivery/build_cookbook/metadata.rb
@@ -0,0 +1,8 @@
+name 'build_cookbook'
+maintainer 'Jeremy Custenborder'
+maintainer_email 'jcustenborder@gmail.com'
+license 'apachev2'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+depends 'delivery-truck'
