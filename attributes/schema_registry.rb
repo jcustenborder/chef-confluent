26c1507f1c354cd934221f99d5c5c346679bb7fc
Jeremy Custenborder <jcustenborder@gmail.com>
2017-12-14 15:50:10 -0600

    Added schema-registry support.

--- attributes/schema_registry.rb
+++ attributes/schema_registry.rb
@@ -1,12 +1,61 @@
-default['confluent']['schema_registry_user'] = 'schema-registry'
-default['confluent']['schema_registry_data_dir'] = '/var/lib/schema-registry'
-default['confluent']['schema_registry_log_dir'] = '/var/log/schema-registry'
-default['confluent']['schema_registry_service'] = 'schema-registry'
-default['confluent']['schema_registry_systemd_unit'] = File.join(
-    default['confluent']['systemd_dir'],
-    "#{default['confluent']['schema_registry_service']}.service"
-)
-default['confluent']['schema_registry_environment_file'] = File.join(
-    default['confluent']['environment_dir'],
-    default['confluent']['schema_registry_service']
-)
+default['confluent']['schema_registry']['user']              = 'schema-registry'
+default['confluent']['schema_registry']['config_file_owner'] = 'root'
+default['confluent']['schema_registry']['config_file_mode']  = '0644'
+default['confluent']['schema_registry']['config_file']       = '/etc/schema-registry/schema-registry.properties'
+
+default['confluent']['schema_registry']['logging_config_file_owner'] = 'root'
+default['confluent']['schema_registry']['logging_config_file_mode']  = '0644'
+default['confluent']['schema_registry']['logging_config_file']       = '/etc/schema-registry/schema-registry.logging.properties'
+default['confluent']['schema_registry']['log_dir']                        = '/var/log/schema-registry'
+default['confluent']['schema_registry']['log_dir_mode']                   = '0755'
+default['confluent']['schema_registry']['service']                        = 'schema-registry'
+default['confluent']['schema_registry']['service_action']                 = [:enable, :start]
+default['confluent']['schema_registry']['systemd_unit']                   =
+    case node['platform_family']
+      when 'debian'
+        '/lib/systemd/system/schema-registry.service'
+      when 'rhel'
+        '/usr/lib/systemd/system/schema-registry.service'
+    end
+default['confluent']['schema_registry']['systemd_unit_mode']              = '0644'
+default['confluent']['schema_registry']['systemd_unit_owner']             = 'root'
+default['confluent']['schema_registry']['systemd_unit_group']             = 'root'
+default['confluent']['schema_registry']['systemd_service_limitnofile']    = 65536
+default['confluent']['schema_registry']['systemd_service_timeoutstopsec'] = 300
+
+
+default['confluent']['schema_registry']['environment_file']       =
+    case node['platform_family']
+      when 'debian'
+        '/etc/default/schema-registry'
+      when 'rhel'
+        '/etc/sysconfig/schema-registry'
+    end
+default['confluent']['schema_registry']['environment_file_owner'] = 'root'
+default['confluent']['schema_registry']['environment_file_group'] = 'root'
+default['confluent']['schema_registry']['environment_file_mode']  = '0644'
+default['confluent']['schema_registry']['heap_opts']              = '-Xmx1000M'
+default['confluent']['schema_registry']['kafka_opts']             = '-Djava.net.preferIPv4Stack=true'
+
+default['confluent']['schema_registry']['environment_config'] = {
+    'KAFKA_OPTS'     => '-Djava.net.preferIPv4Stack=true',
+    'GC_LOG_ENABLED' => true,
+}
+
+default['confluent']['schema_registry']['config'] = {
+    'listeners'                 => 'http://0.0.0.0:8081',
+    'kafkastore.topic'          => '_schemas',
+    'debug'                     => false
+}
+
+default['confluent']['schema_registry']['logging_config'] = {
+    'log4j.rootLogger'                               => 'INFO, stdout, roller',
+    'log4j.appender.stdout'                          => 'org.apache.log4j.ConsoleAppender',
+    'log4j.appender.stdout.layout'                   => 'org.apache.log4j.PatternLayout',
+    'log4j.appender.stdout.layout.ConversionPattern' => '[%d] %p %m (%c)%n',
+    'log4j.appender.roller'                          => 'org.apache.log4j.DailyRollingFileAppender',
+    'log4j.appender.roller.DatePattern'              => "'.'yyyy-MM-dd-HH",
+    'log4j.appender.roller.File'                     => '${kafka.logs.dir}/server.log',
+    'log4j.appender.roller.layout'                   => 'org.apache.log4j.PatternLayout',
+    'log4j.appender.roller.layout.ConversionPattern' => '[%d] %p %m (%c)%n',
+}
---
+++ recipes/schema_registry.rb
@@ -0,0 +1,70 @@
+#
+# Cookbook:: confluent
+# Recipe:: default
+#
+# Copyright:: 2017, Jeremy Custenborder
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'chef/application'
+
+include_recipe('confluent::default')
+
+unless node['confluent']['schema_registry']['kafkastore_connection_url']
+  Chef::Application.fatal!("node['confluent']['schema_registry']['kafkastore_connection_url'] must be specified.")
+end
+
+user node['confluent']['schema_registry']['user'] do
+  comment 'Service Account for schema_registry'
+  system true
+end
+
+directory node['confluent']['schema_registry']['log_dir'] do
+  owner node['confluent']['schema_registry']['user']
+  group node['confluent']['schema_registry']['user']
+  mode node['confluent']['schema_registry']['log_dir_mode']
+  recursive true
+end
+
+template node['confluent']['schema_registry']['config_file'] do
+  owner node['confluent']['schema_registry']['config_file_owner']
+  group 'root'
+  mode node['confluent']['schema_registry']['config_file_mode']
+  source 'schema_registry/schema-registry.properties.erb'
+end
+
+template node['confluent']['schema_registry']['logging_config_file'] do
+  owner node['confluent']['schema_registry']['logging_config_file_owner']
+  group 'root'
+  mode node['confluent']['schema_registry']['logging_config_file_mode']
+  source 'schema_registry/logging.config.properties.erb'
+end
+
+template node['confluent']['schema_registry']['environment_file'] do
+  owner node['confluent']['schema_registry']['environment_file_owner']
+  group node['confluent']['schema_registry']['environment_file_group']
+  mode node['confluent']['schema_registry']['environment_file_mode']
+  source 'schema_registry/environment.erb'
+end
+
+template node['confluent']['schema_registry']['systemd_unit'] do
+  owner node['confluent']['schema_registry']['environment_file_owner']
+  group node['confluent']['schema_registry']['environment_file_group']
+  mode node['confluent']['schema_registry']['environment_file_mode']
+  source 'schema_registry/systemd.erb'
+  notifies :run, 'execute[systemctl-daemon-reload]', :immediately
+end
+
+service node['confluent']['schema_registry']['service'] do
+  action node['confluent']['schema_registry']['service_action']
+end
